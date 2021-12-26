cdd6248f1d0d384ddaa712883ca642d0b2f02cfa
David <6505887+david-popa@users.noreply.github.com>
2021-12-26 04:40:55 +0100

    Led off when pc off

--- keyboards/ajazz/ak33/rev1/config.h
+++ keyboards/ajazz/ak33/rev1/config.h
@@ -51,7 +51,7 @@
 /* Enable NKRO - Up to 248 keys at the same time. */
 #define FORCE_NKRO
 
-#define RGBLIGHT_SLEEP PCOFF
+#define RGBLIGHT_SLEEP
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 //#define LOCKING_SUPPORT_ENABLE
